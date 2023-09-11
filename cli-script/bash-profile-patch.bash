2581783562993ac58cf4206dde8904b7ab26bee8
yt2nj <138998509+yt2nj@users.noreply.github.com>
2023-09-11 15:08:38 +0800

    add utf-8

--- cli-script/bash-profile-patch.bash
+++ cli-script/bash-profile-patch.bash
@@ -1,1 +1,3 @@
+# if [ -f $HOME/.bashrc ]; then source $HOME/.bashrc; fi;
+export LANG='C.UTF-8'
 PS1='\[\e[35;1m\]\D{%H:%M:%S}\[\e[0m\] \[\e[32;1m\]\u\[\e[0m\]@\[\e[34;1m\]\h\[\e[0m\]:\[\e[33;1m\]\W\[\e[0m\] \[\e[31;1m\]\$\[\e[0m\] '
