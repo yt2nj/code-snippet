d1a795e094f65bf6ba16e8494588186a146b3209
yt2nj <138998509+yt2nj@users.noreply.github.com>
2023-12-11 16:43:57 +0800

    Update bash-profile-patch.bash

--- cli-script/bash-profile-patch.bash
+++ cli-script/bash-profile-patch.bash
@@ -1,3 +1,5 @@
 # if [ -f $HOME/.bashrc ]; then source $HOME/.bashrc; fi;
 export LANG='C.UTF-8'
 PS1='\[\e[35;1m\]\D{%H:%M:%S}\[\e[0m\] \[\e[32;1m\]\u\[\e[0m\]@\[\e[34;1m\]\h\[\e[0m\]:\[\e[33;1m\]\W\[\e[0m\] \[\e[31;1m\]\$\[\e[0m\] '
+
+alias gl="git log --graph --abbrev-commit --date=relative --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cr)%Creset%Cblue%d%Creset'"
